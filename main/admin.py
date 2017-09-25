0d8c6a99b9a4303c1515d332aaa2f1a586f61bc5
Thebreak35 <3gthebreak@gmail.com>
2017-09-25 19:14:45 +0700

    create table Size, Supplier, Order, Customer

--- main/admin.py
+++ main/admin.py
@@ -3,8 +3,12 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Size, Supplier, Order, Customer
 
 # Register your models here.
 
 admin.site.register(Product)
+admin.site.register(Size)
+admin.site.register(Supplier)
+admin.site.register(Order)
+admin.site.register(Customer)
--- main/models.py
+++ main/models.py
@@ -2,20 +2,63 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Product(models.Model):
 	product_Name = models.CharField(max_length=75)
 	product_Detail = models.CharField(max_length=200)
-	product_ID = models.IntegerField()
+	product_ID = models.CharField(max_length=45)
 	category_ID = models.CharField(max_length=45)
-	unit_in_stock = models.IntegerField()
+	unit_in_stock = models.IntegerField(default=0)
 	unit_Price = models.IntegerField()
 	color = models.CharField(max_length=45)
 
-	class Meta:
-		verbose_name_plural = 'products'
+	def __str__(self):
+		return self.product_ID
+
+class Size(models.Model):
+	product_ID = models.ForeignKey('Product')
+	size_06 = models.IntegerField(default=0)
+	size_07 = models.IntegerField(default=0)
+	size_08 = models.IntegerField(default=0)
+	size_09 = models.IntegerField(default=0)
+	size_10 = models.IntegerField(default=0)
+	size_11 = models.IntegerField(default=0)
+	size_12 = models.IntegerField(default=0)
+	size_13 = models.IntegerField(default=0)
 
 	def __str__(self):
-		return self.product_id
+		return self.product_ID
 
+class Supplier(models.Model):
+	supplier_ID = models.ForeignKey('Product')
+	company_name = models.CharField(max_length=45)
+	email = models.CharField(max_length=45)
+	phone = models.CharField(max_length=45)
+	logo = models.CharField(max_length=45)
+
+	def __str__(self):
+		return self.supplier_ID
+
+class Order(models.Model):
+	order_ID = models.CharField(max_length=45)
+	customer_ID = models.CharField(max_length=45)
+	order_Date = models.DateTimeField('date ordered')
+	payment_Detail = models.CharField(max_length=45)
+
+	def __str__(self):
+		return self.order_ID
+
+class Customer(models.Model):
+	customer_ID = models.CharField(max_length=45)
+	First_Name = models.CharField(max_length=45)
+	Last_Name = models.CharField(max_length=45)
+	Address = models.CharField(max_length=45)
+	Password = models.CharField(max_length=45)
+	History = models.CharField(max_length=45)
+	email = models.CharField(max_length=45)
+	product_Name = models.CharField(max_length=45)
+
+	def __str__(self):
+		return self.customer_ID
