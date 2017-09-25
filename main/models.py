3b1aaad441ad57283348971f2c511d3fcae4957f
Thebreak35 <3gthebreak@gmail.com>
2017-09-25 14:07:14 +0700

    Add database table in models.py and admin

--- main/models.py
+++ main/models.py
@@ -4,3 +4,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+	product_Name = models.CharField(max_length=75)
+	product_Detail = models.CharField(max_length=200)
+	product_ID = models.IntegerField()
+	category_ID = models.CharField(max_length=45)
+	unit_in_stock = models.IntegerField()
+	unit_Price = models.IntegerField()
+	color = models.CharField(max_length=45)
+
+	class Meta:
+		verbose_name_plural = 'products'
+
+	def __str__(self):
+		return self.product_id
+
