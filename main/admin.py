49aadf8ad1bb9fdc17437b35449dfff765f43a37
Thebreak35 <3gthebreak@gmail.com>
2017-09-26 13:50:22 +0700

    edit more database

--- main/admin.py
+++ main/admin.py
@@ -3,12 +3,13 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Product, Size, Supplier, Order, Customer
+from .models import *
 
 # Register your models here.
 
 admin.site.register(Product)
-admin.site.register(Size)
 admin.site.register(Supplier)
-admin.site.register(Order)
+admin.site.register(OrderBy)
+admin.site.register(OrderList)
 admin.site.register(Customer)
+admin.site.register(OrderTemp)
--- main/models.py
+++ main/models.py
@@ -4,51 +4,69 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 
+from django import forms
+
 # Create your models here.
 class Product(models.Model):
-	product_Name = models.CharField(max_length=75)
-	product_Detail = models.CharField(max_length=200)
-	product_ID = models.CharField(max_length=45)
-	category_ID = models.CharField(max_length=45)
-	unit_in_stock = models.IntegerField(default=0)
+	product_ID = models.CharField(max_length=45,default='')
+	product_Name = models.CharField(max_length=75,default='')
+	product_Description = models.CharField(max_length=200,default='')
+	gender = forms.ChoiceField(choices=[('Male','Male'),
+										('Female','Female'),
+										('All','All')])
+	brand = models.CharField(max_length=45,default='')
+	color = models.CharField(max_length=45,default='')
 	unit_Price = models.IntegerField()
-	color = models.CharField(max_length=45)
-
-	def __str__(self):
-		return self.product_ID
-
-class Size(models.Model):
-	product_ID = models.ForeignKey('Product')
+	picture = models.CharField(max_length=200,default='')
+	weight = models.IntegerField(default=0)
 	size_06 = models.IntegerField(default=0)
+	size_06_5 = models.IntegerField(default=0)
 	size_07 = models.IntegerField(default=0)
+	size_07_5 = models.IntegerField(default=0)
 	size_08 = models.IntegerField(default=0)
+	size_08_5 = models.IntegerField(default=0)
 	size_09 = models.IntegerField(default=0)
+	size_09_5 = models.IntegerField(default=0)
 	size_10 = models.IntegerField(default=0)
+	size_10_5 = models.IntegerField(default=0)
 	size_11 = models.IntegerField(default=0)
+	size_11_5 = models.IntegerField(default=0)
 	size_12 = models.IntegerField(default=0)
+	size_12_5 = models.IntegerField(default=0)
 	size_13 = models.IntegerField(default=0)
+	size_13_5 = models.IntegerField(default=0)
 
 	def __str__(self):
 		return self.product_ID
 
 class Supplier(models.Model):
-	supplier_ID = models.ForeignKey('Product')
-	company_name = models.CharField(max_length=45)
+	brand = models.CharField(max_length=45)
 	email = models.CharField(max_length=45)
 	phone = models.CharField(max_length=45)
-	logo = models.CharField(max_length=45)
+	logo = models.CharField(max_length=200)
 
 	def __str__(self):
-		return self.supplier_ID
+		return self.brand
 
-class Order(models.Model):
+class OrderBy(models.Model):
 	order_ID = models.CharField(max_length=45)
-	customer_ID = models.CharField(max_length=45)
+	customer_ID = models.ForeignKey('Customer')
 	order_Date = models.DateTimeField('date ordered')
-	payment_Detail = models.CharField(max_length=45)
+	address_to_send = models.CharField(max_length=200)
+	paid = models.BooleanField()
+	date_send = models.DateTimeField('date delivered')
 
 	def __str__(self):
 		return self.order_ID
+
+class OrderList(models.Model):
+	order_ID = models.ForeignKey('OrderBy')
+	product_ID = models.ForeignKey('Product')
+	size = models.IntegerField()
+	unit = models.IntegerField()
+
+	def __str__(self):
+		return str(self.order_ID)
 
 class Customer(models.Model):
 	customer_ID = models.CharField(max_length=45)
@@ -56,9 +74,18 @@ class Customer(models.Model):
 	Last_Name = models.CharField(max_length=45)
 	Address = models.CharField(max_length=45)
 	Password = models.CharField(max_length=45)
-	History = models.CharField(max_length=45)
 	email = models.CharField(max_length=45)
-	product_Name = models.CharField(max_length=45)
+	stripe_key = models.CharField(max_length=45)
 
 	def __str__(self):
 		return self.customer_ID
+
+class OrderTemp(models.Model):
+	customer_ID = models.ForeignKey('Customer')
+	order_ID = models.ForeignKey('OrderBy')
+	product_ID = models.ForeignKey('Product')
+	size = models.IntegerField()
+	unit = models.IntegerField()
+
+	def __str__(self):
+		return str(self.customer_ID)
